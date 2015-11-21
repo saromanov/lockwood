c9aa054b12fa3da0bcb50718edc08fe7057a3d94
Sergey Romanov <xxsmotur@gmail.com>
2015-11-22 02:29:19 +0500

    Http server puppet

--- commands/http_server.go
+++ commands/http_server.go
@@ -8,14 +8,16 @@ import (
 
 type HttpServer struct{
 	Addr string
-	
+
 }
 
+// Register provides registration of all handlers
 func (hs *HttpServer) Register() {
 	http.HandleFunc("/v1/serfstat", hs.serfstat)
 	http.HandleFunc("/v1/send", hs.send)
 }
 
+// Run provides starting of server
 func (hs *HttpServer) Run() {
 	server := http.Server {
 		Addr: hs.Addr,
